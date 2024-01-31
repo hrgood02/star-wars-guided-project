611c23ddcb394b333a4552dbf67609dd58535c59
Hannah Good <hrgood02@gmail.com>
2024-01-31 16:23:43 -0500

    added onclick functionality

--- react-app/src/Character.jsx
+++ react-app/src/Character.jsx
@@ -3,7 +3,7 @@ import './Character.css';
 export function Character({characters}) {
     return (
       <div>
-{characters.map((character) => <button type="button" className="character">{character.name}</button>)}
+{characters.map((character) => <button type="button" className="character" onClick={()=>alert('hi')}>{character.name}</button>)}
       </div>
         
 
